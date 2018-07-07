4313577015ea4715acabf111aa1a4aa0db2e901e
Aaranan1995 <40911001+Aaranan1995@users.noreply.github.com>
2018-07-07 10:29:41 +0200

    Create voting.sol

---
+++ voting.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.4.18;
+
+contract Voting {
+
+    event AddedCandidate(uint candidateID);
+    
+    struct Voter {
+        bytes32 uid; 
+        uint candidateIDVote;
+    }
+    
+    struct Candidate {
+        bytes32 name;
+        bytes32 party; 
+        bool doesExist; 
+    }
+    
+    uint numCandidates; // declares a state variable - number Of Candidates
+    uint numVoters;
+    
+    mapping (uint => Candidate) candidates;
+    mapping (uint => Voter) voters;
+
+    function addCandidate(bytes32 name, bytes32 party) public {
+        uint candidateID = numCandidates++;
+        candidates[candidateID] = Candidate(name,party,true);
+        AddedCandidate(candidateID);
+    }
+
+    function vote(bytes32 uid, uint candidateID) public {
+        if (candidates[candidateID].doesExist == true) {
+            uint voterID = numVoters++; //voterID is the return variable
+            voters[voterID] = Voter(uid,candidateID);
+        }
+    }
+    
+    function totalVotes(uint candidateID) view public returns (uint) {
+        uint numOfVotes = 0; // we will return this
+        for (uint i = 0; i < numVoters; i++) {
+            // if the voter votes for this specific candidate, we increment the number
+            if (voters[i].candidateIDVote == candidateID) {
+                numOfVotes++;
+            }
+        }
+        return numOfVotes; 
+    }
+
+    function getNumOfCandidates() public view returns(uint) {
+        return numCandidates;
+    }
+
+    function getNumOfVoters() public view returns(uint) {
+        return numVoters;
+    }
+
+    function getCandidate(uint candidateID) public view returns (uint,bytes32, bytes32) {
+        return (candidateID,candidates[candidateID].name,candidates[candidateID].party);
+    }
+}
